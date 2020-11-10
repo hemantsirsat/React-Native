44ac56f1ea7389bd2e983619617d77ba484fa4b4
hemantsirsat <hemantsirsat40@gmail.com>
2020-11-10 23:08:08 +0530

    FlatList, Fixed missing key using keyExtractor

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet, FlatList} from 'react-native';
 
 const ListScreen = () => {
-    return <Text>List Screen</Text>
+    const movies = [
+        {Name: 'Interstallar'},
+        {Name: 'About Time'},
+        {Name: 'The Theory of EveryThing'},
+        {Name: 'The Avengers'},
+        {Name: 'Hulk'},
+        {Name: 'Notting Hill'},
+    ];
+
+    return (
+            <FlatList 
+            keyExtractor = {movie => movie.Name}
+                data = { movies }
+                renderItem={({ item }) =>  {
+                    return <Text>{item.Name}</Text>;
+                }}
+            />      
+            );
 };
 
 const style = StyleSheet.create({});
