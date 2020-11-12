bad0f2f1773589af9a88c7e6cfa0c281893a70be
hemantsirsat <hemantsirsat40@gmail.com>
2020-11-12 20:33:43 +0530

    Counter Application: using useState

---
+++ src/screens/CounterScreen.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+
+const CounterScreen = ()=> {
+    const [counter, setCounter] = useState(0);
+    return(
+        <View>
+            <Button 
+                title="Increase"
+                onPress={()=>{
+                    //Don't do this :counter++;
+                    setCounter(counter + 1);
+                }}    
+            />
+            <Button 
+                title="Decrease"
+                onPress={()=>{
+                    //Don't do this :counter--;
+                    setCounter(counter-1);
+                }}
+            />
+            <Text>Counter Count: {counter}</Text>
+        </View>
+    
+    );
+    
+};
+
+const styles = StyleSheet.create({});
+
+export default CounterScreen;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -18,6 +18,10 @@ const HomeScreen = ({navigation}) => {
         title="Go to ImageScreen"
         onPress = { ()=> navigation.navigate('Images')}
       />
+      <Button
+        title="Go to Counter"
+        onPress = { ()=> navigation.navigate('Counter')}
+      />
     </View>
   );
 };
