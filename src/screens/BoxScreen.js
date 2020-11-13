d80fe59e7c4ca94ed54144ab2f901caa243b5663
hemantsirsat <hemantsirsat40@gmail.com>
2020-11-13 14:45:43 +0530

    Styling: alignItems(flex-start,center,flex-end)

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,21 +4,24 @@ import { Text, View, StyleSheet } from 'react-native';
 const BoxScreen = () => {
     return (
         <View style={style.viewStyle}>
-            <Text style={style.textStyle}>Box Screen</Text>
+            <Text style={style.textStyle}>Child 1</Text>
+            <Text style={style.textStyle}>Child 2</Text>
+            <Text style={style.textStyle}>Child 3</Text>
         </View>
     );
 };
 
 const style = StyleSheet.create({
     textStyle:{
-        margin:20,
+        margin:10,
         borderWidth:2,
         borderColor:'blue',
-        padding:10
+        padding:5
     },
     viewStyle:{
-        borderWidth:1,
-        borderColor:'red'
+        borderWidth:3,
+        borderColor:'red',
+        alignItems:'center'
     }
 });
 
